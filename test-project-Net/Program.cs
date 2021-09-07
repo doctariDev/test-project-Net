07f9767512a3ff2ad5a68b0a22c91137fc9da321
Lutz.Brandt <lutz.brandt@doctari.de>
2021-09-07 14:21:27 +0200

    task description

--- test-project-Net/Program.cs
+++ test-project-Net/Program.cs
@@ -2,15 +2,22 @@
 
 namespace test_project_Net
 {
+    /// <summary>
+    /// start class of test project
+    /// </summary>
     class Program
     {
+
+        /// <summary>
+        /// use DataReader to get content
+        /// and display with console
+        /// </summary>
         static void Main(string[] args)
         {
             var dataReader = new DataReader();
             var content = dataReader.ReadData();
 
             Console.WriteLine(content);
-
 
             Console.WriteLine();
             Console.WriteLine("Press any key to continue");
